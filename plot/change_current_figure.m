e9dc254c30efaee1189f54be118df4ed80bef3ac
Moritz Schappler <moritz.schappler@imes.uni-hannover.de>
2019-08-27 13:49:07 +0100

    [Plot] Funktion zum Wechseln des Figures ohne Fokus-Klau

---
+++ plot/change_current_figure.m
@@ -0,0 +1,20 @@
+% Wechsle zu einem neuen Fenster ohne den Fokus auf Matlab zu ziehen
+% Nützlich für Skripte, in denen viele Bilder generiert werden, während
+% gleichzeitig am Rechner gearbeitet wird.
+% 
+% Eingabe:
+% h
+%   Nummer des Figure-Handles (Sollte in Plot-Skripten auf einen
+%   definierten Wert gesetzt werden).
+
+% https://stackoverflow.com/questions/8488758/inhibit-matlab-window-focus-stealing
+
+% Moritz Schappler, moritz.schappler@imes.uni-hannover.de, 2019-08
+% (C) Institut für Mechatronische Systeme, Universität Hannover
+
+function change_current_figure(h)
+try
+  set(0,'CurrentFigure',h);
+catch
+  figure(h);
+end
