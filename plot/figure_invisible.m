c9cefcf640eb5353b7baa8f961d80c3c7da31c5c
Moritz Schappler <moritz.schappler@imes.uni-hannover.de>
2023-08-04 10:39:34 +0200

    Kommentar ergänzt

--- plot/figure_invisible.m
+++ plot/figure_invisible.m
@@ -1,5 +1,7 @@
 % Create a new Matlab figure in invisible mode to avoid focus stealing
 % https://de.mathworks.com/matlabcentral/answers/98788-is-it-possible-to-create-multiple-figures-in-background-such-that-it-does-not-steal-focus-in-matlab
+% 
+% See also: make_figures_visible.m
 
 % Moritz Schappler, moritz.schappler@imes.uni-hannover.de, 2022-01
 % (C) Institut für Mechatronische Systeme, Leibniz Universität Hannover
