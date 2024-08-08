2334ae40de22cc989f08f021d545b7e49a61af31
Moritz Schappler <moritz.schappler@imes.uni-hannover.de>
2024-08-08 09:04:23 +0200

    Plot-Vorlage: Funktion dient nun lediglich zur Anwendung der Vorlage und erstellt den Plot nicht mehr direkt. Dadurch wird die Anwendung deutlich erleichtert und vielseitiger
    Squashed commit of the following:
    
    commit 66f76a7b634252659e0f96caa727479d9db14547
    Author: Björn Volkmann <bjoern.volkmann@imes.uni-hannover.de>
    Date:   Thu Nov 3 14:19:26 2022 +0100
    
        Funktion dient nun lediglich zur Anwendung der Vorlage und erstellt den Plot nicht mehr direkt. Dadurch wird die Anwendung deutlich erleichtert und vielseitiger
    
    commit 19310a54e5ef24cb5f6021fb15c48370b8eb604c
    Author: Björn Volkmann <bjoern.volkmann@imes.uni-hannover.de>
    Date:   Wed Dec 8 10:59:41 2021 +0100
    
        Korrekturen für min/max Werte der Achsen und Legendenposition
    
    commit d671df71370836af59ed4cc481d2311ce878353d
    Author: Björn Volkmann <bjoern.volkmann@imes.uni-hannover.de>
    Date:   Wed Dec 8 10:13:26 2021 +0100
    
        Hinzufügen von Funktionen zum erstellen von Plots nach imes-Template

--- plot/imes_plot_template_simple.m
+++ plot/imes_plot_template_simple.m
@@ -1,38 +1,12 @@
-function f = imes_plot_template_simple(xData, XLabel, yData, YLabel, DataStyle, DataColors, DataName, LegendPos)
+function f = imes_plot_template_simple(f)
 
-% Erstellt nach vorgabe des imes-Template
+% Diese Funktion nimmt einen Plot f und wended auf diesen die imes
+% Formatvorlage an. Dabei werden die Größe des Fensters, die Achsen die
+% Legende entsprechend formatiert. Farben und Textinhalt bleiben
+% unverändert.
 %
-% Die Fuktion gibt das figureObj zurück
+% f         Figure Handle
 %
-% xData         Datenreihen für die x-Achse
-%               Übergabe entweder als matrix aus Spaltenvektoren oder cell 
-%               aus Spaltenvektoren, wenn die Datenreihen unterschiedlich
-%               lang sind
-%
-% XLabel        Label der x-Achse als string
-%
-% yData         Datenreihen für die y-Achse
-%               Übergabe entweder als matrix aus Spaltenvektoren oder cell 
-%               aus Spaltenvektoren, wenn die Datenreihen unterschiedlich
-%               lang sind
-%
-% YLabel        Label der y-Achse als string
-%
-% DataSyle      Stil der Datenreihe
-%               Übergabe als array aus strings. Die strings entsprechen der
-%               üblichen Matlab-Formatierung, also '-', '--' usw.
-%
-% DataColors    Farben der Datenreihen
-%               Übergabe als cell aus RGB array
-%
-% DataName      Namen der Datenreihen für die Legende
-%               Übergabe als cell array aus strings. Wenn keine Legende
-%               erstellt werden soll, muss eine leer cell {} übergeben
-%               werden.
-%
-% LegendPos     Position der Legende im plot
-%               Übergabe als nach Matlab-Konvention für LegendPosition()
-% 
 % Björn Volkmann, bjoern.volkmann@uni-hannover.de
 % Institut für mechatronische Systeme, Universität Hannover
 
@@ -44,14 +18,14 @@ schrift.groesse = 11;
 laengen.breite_fig           = 12;
 laengen.hoehe_fig            = 8;
 
-laengen.offset_breite_links  = 2;
-laengen.offset_breite_rechts = 0.5;
+laengen.offset_breite_links  = 1.5;
+laengen.offset_breite_rechts = 0.8;
 laengen.offset_oben          = 1.5;
 laengen.Abstand_plots        = 0.1;
 laengen.hoehe_label          = 1.2;
+
 % Figure aufrufen
-f = figure(1);
-clf(1);
+% set(1, 'CurrentFigure', f)
 
 set(f,'DefaultAxesUnit','centimeters')
 set(f,'DefaultAxesFontName',schrift.art)
@@ -84,46 +58,16 @@ laengen.hoehe_axes     = (laengen.hoehe_fig-laengen.offset_oben-(n-1)*laengen.Ab
 
 positionen(1).pos      = [laengen.offset_breite_links   laengen.hoehe_fig-(laengen.offset_oben + laengen.hoehe_axes)  laengen.breite_axes     laengen.hoehe_axes];
 
-%% Plot 1
+%% Axen
 
-achsen.a = axes;
-achsen.a.Position   = positionen(1).pos; 
+a = findobj(f,'type','axes');
+a.Units = 'centimeters';
+a.Position   = positionen(1).pos; 
 
+a.XGrid       = 'on';
+a.YGrid       = 'on';
 
-for i = 1:size(yData, 2)
-    
-    if iscell(xData)
-        plot(xData{i}, yData{i}, "LineStyle", DataStyle(i), "LineWidth", 1.5, "Color", DataColors{i})
-        hold on
-    else
-        plot(xData(:,i), yData(:,i), "LineStyle", DataStyle(i), "LineWidth", 1.5, "Color", DataColors{i})
-        hold on
-    end
-    
-end
-hold off
-
-space = max(yData, [],'all') - min(yData, [],'all');
-limits.ymin            = min(yData, [],'all') - space * 0.1;
-limits.ymax            = max(yData, [],'all') + space * 0.1;    
-limits.xmin            = min(xData, [],'all');
-limits.xmax            = max(xData, [],'all');
-
-achsen.a.XLim        = [limits.xmin limits.xmax];
-achsen.a.YLim        = [limits.ymin limits.ymax];
-achsen.a.XGrid       = 'on';
-achsen.a.YGrid       = 'on';
-
-label.y              = ylabel(YLabel);
-label.x              = xlabel(XLabel);
-
-if ~isempty(DataName)
-legenden.l            = legend(DataName);
-legenden.l.Units      = 'centimeters';
-legenden.l.Position   = [10.2 1.8 0 0];
-legenden.l.Location   = LegendPos;
-
-legenden.l.Box        = 'off';
-end
+l            = findobj(f,'type','legend');
+l.Box        = 'off';
 
 
